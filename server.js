2aeaedc0c682b574f769a5e832e142c3adb56b2f
William Flaherty <william.flaherty@gmail.com>
2020-12-10 10:56:54 -0600

    adding stuff

--- server.js
+++ server.js
@@ -1,23 +1,93 @@
 const mongoose = require('mongoose');
-const mongoDB = 'mongodb+srv://lea_admin_willie:Dndmongodb_forlea44!@lea0-hs7rh.mongodb.net/to_do_list_db?retryWrites=true&w=majority'; 
 var List = require('./models/ToDo.js'); 
 var Item = require('./models/ToDoItem.js'); 
+//Load in the express module
+const express = require('express');  
 
+//creates a new express application 
+const app = express(); 
+//declare the port we want to connect to 
+const port = 3000; 
+
+const mongoDB = 'mongodb+srv://lea_admin_willie:Dndmongodb_forlea44!@lea0-hs7rh.mongodb.net/to_do_list_db?retryWrites=true&w=majority'; 
 mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true}, (err, client) => {
     if(err) return console.error(err);
     console.log('Connected to database'); 
 }); 
-
-
 const db = mongoose.connection;
 //open a connection to the database 
 db.on('error', console.error.bind(console, 'MongoDB connection error: '));
 
-//here 
+//opening up our server to listen on a specific ip address and port 
+//ip addresses are also known as hostnames 
+app.listen(port, function(){
+    console.log("The server is running at port " + port); 
+});
 
+/* our api calls */ 
+/* Retrieve List
+Retrieve Specific Item in List
+*/ 
+
+app.get('/', function(request, response){
+    /*get data*/ 
+    Item.find(function(err, items){
+            if (err) return console.error(err);
+            response.send(items); 
+    });    
+}); 
+
+
+/* Jeff */ 
+///127.0.0.1:3000/medium 
+app.get('/medium', function (request, response) {
+    let myVal = Item.find({
+        itemPriority: "Medium" // traverse through list and find an item by priority
+    },
+    
+    function (error, anArrayOfMediumPriorityItems) {
+        if (error) return console.error(error);
+        response.send(anArrayOfMediumPriorityItems);
+    });
+
+
+});
+
+/*ryan */ 
+//http://127.0.0.1:3000/tasks/5fc90ebc5a6168f0f3f89458
+app.get('/tasks/:id', (request, response) => {
+    let myQuery = Item.findOne({ _id: request.params.id }); 
+
+    //do other stuff 
+    myQuery.exec((err, item) => {
+        if (err) return console.error(err);
+        response.send(item);
+    });
+
+});
+
+app.post('/item', function(request, response){
+    //use the request data to create a new item 
+    //and add it to my database. 
+    console.log(request); 
+    let item1 = new Item({
+        itemName     : "Do Dishes",
+        itemPriority : "High" ,
+        assignee     : "Willie",
+        completed    : false  
+    }); 
+
+    item1.save(function(err, item){
+        if (err) return console.error(err);
+        console.log(item); 
+    }); 
+});
+
+
+
+/* 
 //open another connection to the database 
 db.once('open', function(){
-    //your tutorial and new code go here. 
     console.log("We're connected");
     
     //create a new item instance using the Item model
@@ -32,6 +102,15 @@ db.once('open', function(){
         if (err) return console.error(err);
         console.log(item); 
     }); 
+     //how to add more items to the list
+    item2 = new Item({
+        itemName     : "Walk the dog",
+        itemPriority : "High" ,
+        assignee     : "Willie",
+        completed    : false  
+    }); 
+
+    item2.save(); 
 
     //create new list with our item. 
     var myList = new List({
@@ -39,18 +118,65 @@ db.once('open', function(){
         items : [
             {
             item : item1._id 
+            },
+            {
+            item : item2._id 
             }
             ],
     }); 
 
-    //how to add more items to the list
-    //how to find an item
+    //how to find an item in the item schema 
+    Item.findById(item1._id, function(err, item){
+        console.log(item1); 
+    })
+
+    Item.find({itemName : 'Grocery Shopping'},function (err, items){
+        if (err) return console.error(err);
+        console.log(items);
+        });
+
+    Item.find({
+            itemPriority: "Medium" // traverse through list and find an item by priority
+            }, function(err, items){
+                if (err) return console.error(err);
+                console.log(items)
+            });
+            
+    //finding an item from a list 
+    List.find({item: item1._id}, function(err, items){
+        console.log(items); //what will be inside this object 
+        /*[   {
+        item : ItemID("30980230diuao03")
+            }   ] */
+ /*   }); 
+
+
     //how to update an item
+    Item.findOneAndUpdate({name: 'Do Laundry'}, {priority: 'Low'}, function(err, item){
+        console.log('item updated')
+    });
+
+    Item.findById(item1._id, function (err, item) {
+        if (err) return handleError(err); //they check to see if 
+        //the data is good 
+        
+        item.set({ priority: 'Low' }); //they update the value of the 
+        //size property to large 
+      
+        item.save(function (err, updatedItem) {
+          if (err) return handleError(err); //check for erros in saving
+          console.log(updatedItem); //response to the client with an updated tank
+        }); 
+      });
+
     //how to delete an item 
+    Item.findOneAndDelete({itemId : item1._id}, function(err,item){
+        console.log("deleted: ");
+        console.log(item); 
+    });
 
     myList.save(); //add callback to error check if we wanted
 
     
 });
-
-//or here, we're still connected to the database so we can make changes
+*/ 
